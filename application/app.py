a977796faaf055d4dfe97e5684d7b65cfd27b19b
JohannesFalk99 <johannesfalk99@gmail.com>
2025-10-27 22:56:16 +0100

    ghsf

--- application/app.py
+++ application/app.py
@@ -8,21 +8,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/data')
-def data():
-    return render_template('data.html')
+# @app.route('/data')
+# def data():
+#     return render_template('data.html')
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+# @app.route('/profile')
+# def profile():
+#     return render_template('profile.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html')
 
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
+# @app.route('/settings')
+# def settings():
+#     return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
